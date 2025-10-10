77cc1bd44f1cd6f9d9a7794ab56a801785915635
Emmanuel <narh.emma001@gmail.com>
2025-10-09 21:14:03 -0700

    aio2.py

--- ai.py
+++ ai.py
@@ -1,1 +1,11 @@
+print("hello world")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"hello {name}, you are {age} years old")
+if age >= 18:
+    print("you are eligble to vote")
+else:
+    print("You are not eligble to vote")    
+
 
